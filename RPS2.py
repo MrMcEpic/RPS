e848c63e1eec753de7cfe8869b7c4a942ea5127f
MrMcEpic <MrMcEpic@users.noreply.github.com>
2018-11-19 23:17:34 -0500

    included player 2

--- RPS2.py
+++ RPS2.py
@@ -15,6 +15,7 @@ TODO: Add player 2
 
 
 import random
+import os
 from distutils.util import strtobool
 from end import end_ as end
 
@@ -48,18 +49,29 @@ class Game:
         self.p_2_guess=self.rps_dict[random.randint(1,3)]
     def grab_user(self):
         while True:
-            self.p_1_guess=input("Rock, Paper, or Scissors?{p1}".format(p1=" Player One" if self.p_2_enabled==True else ""))
-            self.p_1_guess=self.p_1_guess.lower()
+            self.p_1_guess=input("Rock, Paper, or Scissors?{p1}".format(p1=" Player One" if self.p_2_enabled==True else "")).lower()
+            #self.p_1_guess=self.p_1_guess.lower()
             if self.p_1_guess in self.rps_dict.values():
                 break
             else:
                 print("Wrong input, please enter Rock, Paper, or Scissors")
-        print("P1:",self.p_1_guess)
+                continue
+        #print("P1:",self.p_1_guess)
         if not self.p_2_enabled:
             self.roll()
             print("CPU:",self.p_2_guess)
         else:
-            pass
+            #code for player two
+            ##ToDo: find way to clear console
+            #print("\n"*100)
+            os.system('cls')
+            while True:
+                self.p_2_guess=input("Rock, Paper, or Scissors? Player Two").lower()
+                if self.p_2_guess in self.rps_dict.values():
+                    break
+                else:
+                    print("Wrong input, please enter Rock, Paper, or Scissors")
+                    continue
         self.logic()
     def logic(self):
         global p1_wins,p2_wins,ties
@@ -75,29 +87,23 @@ class Game:
             ties +=1
         elif p1 == "rock":
             if p2 == "paper":
-                #p2 win
                 win=2
                 p2_wins+=1
             elif p2 == "scissors":
-                #p1 win
                 win=1
                 p1_wins+=1
         elif p1 == "paper":
             if p2 == "scissors":
-                #p2 win
                 win=2
                 p2_wins+=1
             elif p2 == "rock":
-                #p1 win
                 win=1
                 p1_wins+=1
         elif p1 == "paper":
             if p2 == "scissors":
-                #p2 win
                 win=2
                 p2_wins+=1
             elif p2 == "rock":
-                #p1 win
                 win=1
                 p1_wins+=1
         print("{winner}".format(winner="Tie" if  win == 0 else "Player One Wins!" if win == 1 else "CPU Wins!" if self.p_2_enabled==False else "Player Two Wins!"))
@@ -110,30 +116,23 @@ while True:
         start_=input("Would you like to play Rock Paper Scissors?").lower()
         i=strtobool(start_)
         if i:
-            mem=Game()
+            while True:
+                p2_quest=input("Would you like to play 1 player or 2 player? Input 1 or 2").lower()
+                if p2_quest.isdigit():
+                    if p2_quest == "1":
+                        mem=Game()
+                        break
+                    elif p2_quest == "2":
+                        mem=Game(True)
+                        break
+                else:
+                    continue
             break
         elif not i:
             end(5)
             break
     except ValueError:
         print("Pleasse input yes or no")
-
-
-# In[ ]:
-
-
-from pathlib import Path
-d = Path().cwd()
-print(d)
-d=str(d)
-print(d)
-__file__
-
-
-# In[ ]:
-
-
-help(Path().resolve())
 
 
 # In[ ]:
